cc72f96114d7248b91acf96435e62818bcc884fa
wangyu19810820 <gadfly0036_cn@sina.com>
2018-09-07 11:15:47 +0800

    表单

---
+++ form/index.js
@@ -0,0 +1,16 @@
+let vm1 = new Vue({
+    el: '#vm1',
+    data: {
+        textField: undefined,
+        textField1: undefined,
+        textArea: 'textArea',
+        checkBoxValue: '是',
+        checkBoxValueArr: [],
+        radioValue: 'art',
+        optionValueList: ['music', 'sport', 'art'],
+        selectValue: 'sport',
+        selectValueArr: ['sport', 'art'],
+        selectValue1: {num: 1, value: "sport"},
+        selectValueArr1: [{num: 1, value: "sport"}, {num: 2, value: "art"}],
+    }
+})
