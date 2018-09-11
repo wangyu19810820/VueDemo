ac8ee59000f1999d4461b14714ee19b76de95c9d
wangyu19810820 <gadfly0036_cn@sina.com>
2018-09-11 08:27:05 +0800

    vue-cli

---
+++ cli-demo/demo1/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div>
+      <input class="redColor blueBg" v-model="inputValue" />
+      <button @click="addTodo">添加</button>
+    </div>
+    <ul>
+      <hello-world v-for="(todo, i) in todoList"
+                   v-bind:todoValue="todo" v-bind:index="i"
+                   v-on:remove="removeTodo"
+      ></hello-world>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import HelloWord from "./components/HelloWorld";
+
+  export default {
+    name: 'App',
+    data () {
+      return {
+        inputValue: '',
+        todoList: [],
+      }
+    },
+    methods: {
+      addTodo() {
+        this.todoList.push(this.inputValue);
+        this.inputValue = '';
+      },
+      removeTodo(i) {
+        this.todoList.splice(i, 1)
+      },
+    },
+    components: {
+      'hello-world': HelloWord,
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ cli-demo/demo1/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <li @click="remove" class="redColor blueBg">
+    {{ todoValue }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  },
+  props: ['todoValue', 'index'],
+  methods: {
+    remove() {
+      this.$emit('remove', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+  .blueBg {
+    background-color: cornflowerblue;
+  }
+
+
+</style>
+<style scoped>
+
+  .redColor {
+    color: red;
+  }
+
+
+</style>
